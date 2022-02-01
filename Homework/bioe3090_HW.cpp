df785500dd151b754bcde1cad038f2dc5de9ef95
Steve Lammers <StevenLammers12@gmail.com>
2022-01-31 17:09:05 -0700

    On branch development  Changes to be committed: 	modified:   Homework/bioe3090_HW.cpp

--- Homework/bioe3090_HW.cpp
+++ Homework/bioe3090_HW.cpp
@@ -12,7 +12,6 @@
  *
  */
 
-#include "stdafx.h"
 #include<iostream>
 #include <string>
 #include <cstdlib>
@@ -88,7 +87,6 @@ double array_average(double _tmpArr[], int _arrSize)
   // complete the body of the function.
   // hint: maybe a sum and a division here...
   // STUDENT COMPLETE THIS SECTION  << -- HWS2----
-	return 0;
 }
 
 void push_array(double _arr[], int _arrSize, double newVal)
@@ -255,6 +253,5 @@ int main()
   cout << "-----------------------------------------------------\n";
   cout << "-----------------------------------------------------\n";
 
-  return 0;
 
 }
